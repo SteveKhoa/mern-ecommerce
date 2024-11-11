c7ce047751cdb4e82ccdc9f75f137db61c71ab30
Khoa LTN <vietnamkhoale@gmail.com>
2024-11-11 11:07:15 +0700

    fix: rename MERN Ecommerce to Dalat Organics

--- client/webpack/webpack.prod.js
+++ client/webpack/webpack.prod.js
@@ -139,9 +139,9 @@ const config = {
       filename: 'css/[name].[hash].css'
     }),
     new WebpackPwaManifest({
-      name: 'MERN Store',
-      short_name: 'MERNStore',
-      description: 'MERN Store!',
+      name: 'Dalat Organics',
+      short_name: 'DalatOrganics',
+      description: 'Dalat Organics!',
       background_color: '#fff',
       theme_color: '#4a68aa',
       inject: true,
